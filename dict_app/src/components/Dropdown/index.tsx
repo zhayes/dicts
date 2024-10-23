eef96da227d3b97cd3f547175cc1c6599b08a000
zhayaping <zhayaping@outlook.com>
2024-10-23 13:10:42 +0800

    修改词源

--- dict_app/src/components/Dropdown/index.tsx
+++ dict_app/src/components/Dropdown/index.tsx
@@ -62,7 +62,7 @@ const Dropdown: Component<{
                     navigate(`${is_dict==='dict' ? '/word/' : '/dict/'}${item.name}`)
                   }}
                 >
-                  {is_dict==='dict' ? '词源搜索' : '字典查询'}
+                  {is_dict==='dict' ? '辞源搜索' : '词意查询'}
                   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="size-3">
                     <path stroke-linecap="round" stroke-linejoin="round" d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25" />
                   </svg>
